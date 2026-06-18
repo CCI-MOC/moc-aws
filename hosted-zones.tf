06ac3534af83fe645a78a5a0ddd48ef3a23f0c17
Lars Kellogg-Stedman <lars@redhat.com>
2026-06-18 19:54:34 -0400

    Add configuration to manage route53 hosted zones

---
+++ hosted-zones.tf
@@ -0,0 +1,33 @@
+# -----------------------------------------------------------------------------
+# Route53 – Hosted zones
+# -----------------------------------------------------------------------------
+
+resource "aws_route53_zone" "massopen_cloud" {
+  name    = "massopen.cloud"
+  comment = "Top-level MOC domain"
+}
+
+resource "aws_route53_zone" "int_massopen_cloud" {
+  name    = "int.massopen.cloud"
+  comment = "Internal MOC systems"
+}
+
+resource "aws_route53_zone" "svc_massopen_cloud" {
+  name    = "svc.massopen.cloud"
+  comment = "Public facing services"
+}
+
+resource "aws_route53_zone" "box_massopen_cloud" {
+  name    = "box.massopen.cloud"
+  comment = "Used by the OSAC development environment"
+}
+
+resource "aws_route53_zone" "ocp_massopen_cloud" {
+  name    = "ocp.massopen.cloud"
+  comment = "For MOCs OpenShift clusters. Initially they will host the NIST cluster."
+}
+
+resource "aws_route53_zone" "osac_integration_svc_massopen_cloud" {
+  name    = "osac-integration.svc.massopen.cloud"
+  comment = ""
+}
