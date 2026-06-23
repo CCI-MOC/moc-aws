9fa3639f45434158fe7e09e397e1b32cee5093a5
Lars Kellogg-Stedman <lars@redhat.com>
2026-06-23 10:24:08 -0400

    Add support for S3 storage classes
    We can use this to create e.g. a Glacier Flexible Retrieval bucket.

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -11,3 +11,20 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  count  = var.storage_class != null ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    id     = "transition-to-${lower(var.storage_class)}"
+    status = "Enabled"
+
+    filter {}
+
+    transition {
+      days          = 0
+      storage_class = var.storage_class
+    }
+  }
+}
--- modules/bucket/variables.tf
+++ modules/bucket/variables.tf
@@ -8,3 +8,17 @@ variable "allow_public_access" {
   type        = bool
   default     = false
 }
+
+variable "storage_class" {
+  description = "S3 storage class for lifecycle transition (e.g. GLACIER, GLACIER_IR, STANDARD_IA, DEEP_ARCHIVE). When null, no lifecycle rule is created."
+  type        = string
+  default     = null
+
+  validation {
+    condition = var.storage_class == null || contains(
+      ["STANDARD_IA", "ONEZONE_IA", "INTELLIGENT_TIERING", "GLACIER_IR", "GLACIER", "DEEP_ARCHIVE"],
+      var.storage_class
+    )
+    error_message = "storage_class must be one of: STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING, GLACIER_IR, GLACIER, DEEP_ARCHIVE"
+  }
+}
