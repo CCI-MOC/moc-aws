a763b1c8f0052022718a731eed82b4fb527b998e
Lars Kellogg-Stedman <lars@redhat.com>
2026-06-23 22:04:41 -0400

    Add missing descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,13 @@
 # -----------------------------------------------------------------------------
 
 output "sso_instance_arn" {
-  value = local.sso_instance_arn
+  value       = local.sso_instance_arn
+  description = "ARN of our AWS Identity Center instance"
 }
 
 output "identity_store_id" {
-  value = local.identity_store_id
+  value       = local.identity_store_id
+  description = "ARN of the Identity Store instance associated with our IAM Identity Center instance. Used when creating SSO users and groups."
 }
 output "github_actions_admin_role_arn" {
   value       = module.github-oidc.github_actions_admin_role_arn
@@ -20,5 +22,6 @@ output "github_actions_dns_role_arn" {
 }
 
 output "iam_user_access_keys" {
-  value = { for name, user in module.iam_user : name => user.access_keys }
+  value       = { for name, user in module.iam_user : name => user.access_keys }
+  description = "Show names, access key ids, and corresponding secret ARN for all managed iam users"
 }
