774d0e4ed69ee80c7e6309404c4933251afc320c
Lars Kellogg-Stedman <lars@redhat.com>
2026-07-17 10:31:24 -0400

    Add import blocks for existing b2 buckets

---
+++ imports.tf
@@ -0,0 +1,49 @@
+import {
+  to = module.b2.module.bucket["esi-leases"].b2_bucket.this
+  id = "fdd02e51fa0954a59e730612"
+}
+
+import {
+  to = module.b2.module.bucket["nerc-invoicing"].b2_bucket.this
+  id = "ed908e71bab974e58ec30612"
+}
+
+import {
+  to = module.b2.module.bucket["nerc-invoicing-iceberg"].b2_bucket.this
+  id = "dda05e816aa954859ec30612"
+}
+
+import {
+  to = module.b2.module.bucket["nerc-ocp-barcelona-metrics"].b2_bucket.this
+  id = "8de0de61dad9f4e59eb30612"
+}
+
+import {
+  to = module.b2.module.bucket["nerc-ocp-edu-metrics"].b2_bucket.this
+  id = "cd30be31ba89e4c59e830612"
+}
+
+import {
+  to = module.b2.module.bucket["nerc-ocp-edu-metrics-hf"].b2_bucket.this
+  id = "6d304e11ba99e4c59e830612"
+}
+
+import {
+  to = module.b2.module.bucket["nerc-ocp-test"].b2_bucket.this
+  id = "2dd0be613a9904659e530612"
+}
+
+import {
+  to = module.b2.module.bucket["openshift-metrics"].b2_bucket.this
+  id = "8d70ae217a3984358ec30612"
+}
+
+import {
+  to = module.b2.module.bucket["openshift-metrics-backup-source"].b2_bucket.this
+  id = "9d104e514ac984d59e330612"
+}
+
+import {
+  to = module.b2.module.bucket["openshift-metrics-hf"].b2_bucket.this
+  id = "1d109e21ba0994959e830612"
+}
