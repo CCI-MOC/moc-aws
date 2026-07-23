64c7b6cee950b0884df21e36a58a474cb69c747f
Lars Kellogg-Stedman <lars@redhat.com>
2026-07-23 15:33:34 -0400

    Fix unknown value errors
    The previous version of this pr was failing because (a) we were iterating
    over var.policy_arns in `iam_users.tf`, a map for which the values are
    unknown. This by itself is fine, but in `modules/iam_users` we were then
    iterating over `each.value.policy_arns`, and this made tofu unhappy.
    
    The solution implemented here is to create a `policy_arn_map` variable that
    maps static keys to policy arns. This means that the inner loop in the
    module is iterating over these static keys, and only the inner values are
    unknown, which is fine.
    
    Another workaround would have been to first create and apply the policies
    and then create and apply the users -- because during the second apply the
    policies would already exist there would no longer be an unknown value issue.

--- iam-users.tf
+++ iam-users.tf
@@ -23,25 +23,25 @@ module "cert_manager_policy_innabox" {
 locals {
   cert_manager_policies = {
     "cert_manager_policy_infra_ocp_massopen" = {
-      cluster_subdomain   = "infra.ocp.massopen.cloud"
+      cluster_subdomain  = "infra.ocp.massopen.cloud"
       zone_name          = "ocp.massopen.cloud"
       policy_name        = "cert-manager-infra-ocp-massopen-cloud"
       policy_description = "modify records in infra.ocp.massopen.cloud for dns01 challenges."
     }
     "cert_manager_policy_staging_ocp_massopen" = {
-      cluster_subdomain   = "staging.ocp.massopen.cloud"
+      cluster_subdomain  = "staging.ocp.massopen.cloud"
       zone_name          = "ocp.massopen.cloud"
       policy_name        = "cert-manager-staging-ocp-massopen-cloud"
       policy_description = "modify records in staging.ocp.massopen.cloud for dns01 challenges."
     }
     "cert_manager_policy_moc_infra_massopen" = {
-      cluster_subdomain   = "moc-infra.massopen.cloud"
+      cluster_subdomain  = "moc-infra.massopen.cloud"
       zone_name          = "massopen.cloud"
       policy_name        = "cert-manager-moc-infra-massopen-cloud"
       policy_description = "modify records in moc-infra.massopen.cloud for dns01 challenges."
     }
     "cert_manager_policy_esi_massopen" = {
-      cluster_subdomain   = "esi.massopen.cloud"
+      cluster_subdomain  = "esi.massopen.cloud"
       zone_name          = "massopen.cloud"
       policy_name        = "cert-manager-esi-massopen-cloud"
       policy_description = "modify records for esi.massopen.cloud for dns01 challenges."
@@ -50,9 +50,9 @@ locals {
 }
 
 module "cert_manager_policy" {
-  for_each        = local.cert_manager_policies
+  for_each           = local.cert_manager_policies
   source             = "./modules/iam-policy/cert-manager-route53"
-  cluster_subdomain   = each.value.cluster_subdomain
+  cluster_subdomain  = each.value.cluster_subdomain
   zone_name          = each.value.zone_name
   policy_name        = each.value.policy_name
   policy_description = each.value.policy_description
@@ -63,65 +63,65 @@ module "cert_manager_policy" {
 # -----------------------------------------------------------------------------
 
 locals {
+  policy_arn_map = {
+    cert-manager-ocp-massopen-cloud         = module.cert_manager_policy_ocp_massopen.policy_arn
+    cert-manager-box-massopen-cloud         = module.cert_manager_policy_innabox.policy_arn
+    cert-manager-infra-ocp-massopen-cloud   = module.cert_manager_policy["cert_manager_policy_infra_ocp_massopen"].policy_arn
+    cert-manager-staging-ocp-massopen-cloud = module.cert_manager_policy["cert_manager_policy_staging_ocp_massopen"].policy_arn
+    cert-manager-moc-infra-massopen-cloud   = module.cert_manager_policy["cert_manager_policy_moc_infra_massopen"].policy_arn
+    cert-manager-esi-massopen-cloud         = module.cert_manager_policy["cert_manager_policy_esi_massopen"].policy_arn
+  }
+
   iam_users = {
     "cert-manager-ocp-massopen" = {
       access_keys = {
         cert-manager-nist-clusters = "Used by cert-manager in NIST clusters for dns01 challenges"
       }
-      policy_arns = {
-        cert-manager-ocp-massopen-cloud = module.cert_manager_policy_ocp_massopen.policy_arn
-      }
+      policy_names = ["cert-manager-ocp-massopen-cloud"]
     }
     "innabox-dns-manager" = {
       access_keys = {
         innabox-dns = "Used by cert-manager in innabox dev cluster"
         innabox-aap = "Used by AAP in innabox dev cluster"
       }
-      policy_arns = {
-        cert-manager-box-massopen-cloud = module.cert_manager_policy_innabox.policy_arn
-      }
+      policy_names = ["cert-manager-box-massopen-cloud"]
     }
     "cert-manager-infra-ocp-massopen" = {
       access_keys = {
         cert-manager-infra-ocp-massopen = "Used by certmanager in infra.ocp.massopen.cloud for dns01 challenges"
       }
-      policy_arns = {
-        cert-manager-infra-ocp-massopen-cloud = module.cert_manager_policy["cert_manager_policy_infra_ocp_massopen"].policy_arn
-      }
+      policy_names = ["cert-manager-infra-ocp-massopen-cloud"]
     }
-    "cert-manager-staging-ocp-massopen" = { # import it
+    "cert-manager-staging-ocp-massopen" = {
       access_keys = {
         cert-manager-staging-ocp-massopen = "Used by certmanager in staging.ocp.massopen.cloud for dns01 challenges"
       }
-      policy_arns = {
-        cert-manager-staging-ocp-massopen-cloud = module.cert_manager_policy["cert_manager_policy_staging_ocp_massopen"].policy_arn
-      }
+      policy_names = ["cert-manager-staging-ocp-massopen-cloud"]
     }
-    "cert-manager-moc-infra" = { # import it
+    "cert-manager-moc-infra" = {
       access_keys = {
         cert-manager-moc-infra-massopen = "Used by certmanager in moc-infra.massopen.cloud for dns01 challenges"
       }
-      policy_arns = {
-        cert-manager-moc-infra-massopen-cloud = module.cert_manager_policy["cert_manager_policy_moc_infra_massopen"].policy_arn
-      }
+      policy_names = ["cert-manager-moc-infra-massopen-cloud"]
     }
-    "certbot-letsencrypt-esi" = { # import it
+    "certbot-letsencrypt-esi" = {
       access_keys = {
         certbot-letsencrypt-esi = "Used by certbot in ESI cluster for dns01 challenges"
       }
-      policy_arns = {
-        cert-manager-esi-massopen-cloud = module.cert_manager_policy["cert_manager_policy_esi_massopen"].policy_arn
-      }
+      policy_names = ["cert-manager-esi-massopen-cloud"]
     }
   }
 }
 
 module "iam_user" {
-  for_each                       = local.iam_users
-  source                         = "./modules/iam-user"
-  name                           = each.key
-  tags                           = try(each.value.tags, {})
-  access_keys                    = try(each.value.access_keys, {})
-  policy_arns                    = try(each.value.policy_arns, {})
+  for_each    = local.iam_users
+  source      = "./modules/iam-user"
+  name        = each.key
+  tags        = try(each.value.tags, {})
+  access_keys = try(each.value.access_keys, {})
+  policy_arns = {
+    for name in try(each.value.policy_names, []) :
+    name => local.policy_arn_map[name]
+  }
   secret_recovery_window_in_days = try(each.value.secret_recovery_window_in_days, 30)
 }
