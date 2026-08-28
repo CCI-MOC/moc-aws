9dbaf45c01902ce4831db9858cba95e50f8a6d66
Lars Kellogg-Stedman <lars@redhat.com>
2026-08-28 17:10:43 -0400

    Setup zones for production hcp
    We'll use hcp.oac.massopen.cloud for public-facing addresses (primarily api
    and oauth), while internal services (ignition, konnectivity) will use
    hcp-int.oac.massopen.cloud.
    
    We're managing a couple of DNS records here because these zones are
    suitable for management from moc-dns -- these will primarily consist of
    dynamically generated records.

--- REFERENCE.md
+++ REFERENCE.md
@@ -43,6 +43,8 @@
 | [aws_identitystore_group.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/identitystore_group) | resource |
 | [aws_identitystore_group_membership.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/identitystore_group_membership) | resource |
 | [aws_identitystore_user.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/identitystore_user) | resource |
+| [aws_route53_record.hcp-ext-wildcard](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record) | resource |
+| [aws_route53_record.hcp-int-wildcard](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record) | resource |
 | [aws_route53_zone.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_zone) | resource |
 | [aws_s3_bucket.oac_oidc](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket) | resource |
 | [aws_s3_bucket_policy.oac_oidc](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy) | resource |
--- hosted-zones.tf
+++ hosted-zones.tf
@@ -4,10 +4,11 @@
 
 locals {
   hosted_zones = {
-    "massopen.cloud"         = "Top-level MOC domain"
-    "int.massopen.cloud"     = "Internal MOC systems"
-    "ocp.massopen.cloud"     = "For MOCs OpenShift clusters. Initially they will host the NIST cluster."
-    "hcp.oac.massopen.cloud" = "For hosted control plane clusters deployed by oac-prod-infra"
+    "massopen.cloud"             = "Top-level MOC domain"
+    "int.massopen.cloud"         = "Internal MOC systems"
+    "ocp.massopen.cloud"         = "For MOCs OpenShift clusters. Initially they will host the NIST cluster."
+    "hcp.oac.massopen.cloud"     = "For hosted control plane external services"
+    "hcp-int.oac.massopen.cloud" = "For hosted control plane internal services"
   }
 }
 
@@ -16,4 +17,20 @@ resource "aws_route53_zone" "this" {
   name          = each.key
   comment       = each.value
   force_destroy = true
+}
+
+resource "aws_route53_record" "hcp-ext-wildcard" {
+  zone_id = aws_route53_zone.this["hcp.oac.massopen.cloud"].zone_id
+  name    = "*.hcp.oac.massopen.cloud"
+  type    = "A"
+  ttl     = 300
+  records = ["129.10.5.103"]
+}
+
+resource "aws_route53_record" "hcp-int-wildcard" {
+  zone_id = aws_route53_zone.this["hcp-int.oac.massopen.cloud"].zone_id
+  name    = "*.hcp-int.oac.massopen.cloud"
+  type    = "A"
+  ttl     = 300
+  records = ["10.20.9.10"]
 }
