ca7d9e379454dbfeebf380ec2cc24f7930d179d8
Quan Pham <qmpham2019@gmail.com>
2026-09-03 13:24:02 -0400

    Improve compatibility for terraform-docs pre-commit hook
    The `find` command used in the pre-commit hook script was not compatible
    with macOS. The `-printf` option is not available on zsh, the default shell for MACOS
    
    Replaced with `-exec dirname {} \;` to allow compatibility with zsh.

--- scripts/terraform-docs-hook.sh
+++ scripts/terraform-docs-hook.sh
@@ -2,5 +2,5 @@
 
 terraform-docs .
 
-find ./* -name 'main.tf' \! -path ./main.tf -printf '%h\n' |
+find ./* -name 'main.tf' \! -path ./main.tf -exec dirname {} \; |
   xargs -IDIR terraform-docs --config modules/.terraform-docs.yml "DIR"
